904e4eaa1556409c19de373647f721da7fa35541
DaeYeob Kim <kimdyk2@hanmail.net>
2022-10-01 10:56:06 +0900

    typescript initial setup

---
+++ Study/Publications/시원하게 긁어주는 TS/ch 01/settings.js
@@ -0,0 +1,4 @@
+function my_profile(name, age) {
+    console.log("제 이름은 " + name + "이고 제 나이는 " + age + " 입니다.");
+}
+my_profile("Danny", 24);
---
+++ Study/Publications/시원하게 긁어주는 TS/ch 01/settings.ts
@@ -0,0 +1,5 @@
+function my_profile(name: string, age: number) {
+  console.log("제 이름은 " + name + "이고 제 나이는 " + age + " 입니다.");
+}
+
+my_profile("Danny", 24);
