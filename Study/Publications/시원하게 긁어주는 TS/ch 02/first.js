808e74c127270a8b2e39af78f43e7ec8b39d4545
DaeYeob Kim <kimdyk2@hanmail.net>
2022-10-01 14:32:41 +0900

    typescript example1

---
+++ Study/Publications/시원하게 긁어주는 TS/ch 02/first.js
@@ -0,0 +1,8 @@
+var ex1 = "Coffee chats 22.10.01";
+var Talk = /** @class */ (function () {
+    function Talk() {
+    }
+    return Talk;
+}());
+var talk = new Talk();
+talk.name = "Coffee chat";
---
+++ Study/Publications/시원하게 긁어주는 TS/ch 02/first.ts
@@ -0,0 +1,8 @@
+var ex1: string = "Coffee chats 22.10.01";
+
+class Talk {
+  public name: string;
+}
+
+var talk = new Talk();
+talk.name = "Coffee chat";
