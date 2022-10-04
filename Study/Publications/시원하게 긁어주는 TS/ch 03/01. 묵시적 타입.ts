bcdbf2921f05369755e526b535cb2b5f5bbe8928
DaeYeob Kim <kimdyk2@hanmail.net>
2022-10-05 00:02:51 +0900

    implied type

---
+++ Study/Publications/시원하게 긁어주는 TS/ch 03/01. 묵시적 타입.ts
@@ -0,0 +1,14 @@
+var arr = [1, 2, 3];
+
+var a = arr[0];
+var b = arr[1];
+var c = arr[2];
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+var [d, e, f] = ["string", 1234, true]; // 구조분해 할단 (묵시적 타입)
+console.log(d);
+console.log(e);
+console.log(f);
