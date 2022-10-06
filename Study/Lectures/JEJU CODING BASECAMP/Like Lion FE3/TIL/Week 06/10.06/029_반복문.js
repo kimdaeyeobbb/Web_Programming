b9a613ed6f10388de324603cf24b62a824d63cd9
DaeYeob Kim <kimdyk2@hanmail.net>
2022-10-06 13:02:16 +0900

    반복문

---
+++ Study/Lectures/JEJU CODING BASECAMP/Like Lion FE3/TIL/Week 06/10.06/029_반복문.js
@@ -0,0 +1,15 @@
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+// airbnb의 컨벤션에서 권장 
+// IE 전체 버전에서 사용 가능
+for (const key in ['one', 'two']) {
+    console.log(i);
+}
+
+// airbnb의 컨벤션에서 권장
+// IE전체 버전에서 사용불가 (한국사회에서 사용시 유의)
+for (const iterator of object) {
+    console.log(i);
+}
