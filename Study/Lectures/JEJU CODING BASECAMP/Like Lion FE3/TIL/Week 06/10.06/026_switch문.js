cc9a22d1d57992c60bf7a6333d79b51dffd1d018
DaeYeob Kim <kimdyk2@hanmail.net>
2022-10-06 11:09:20 +0900

    switch

---
+++ Study/Lectures/JEJU CODING BASECAMP/Like Lion FE3/TIL/Week 06/10.06/026_switch문.js
@@ -0,0 +1,13 @@
+let value3 = 'three';
+
+switch (value3) {
+    case 'one':
+        console.log(1);
+        break;
+    case 'two':
+        console.log(2);
+        break;
+    case 'three':
+        console.log(3);
+        break;
+}
