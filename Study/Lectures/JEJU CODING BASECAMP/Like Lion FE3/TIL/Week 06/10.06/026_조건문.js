0d99ed4e209a98cacae558f13175db5a57a450fa
DaeYeob Kim <kimdyk2@hanmail.net>
2022-10-06 11:09:26 +0900

    조건문

---
+++ Study/Lectures/JEJU CODING BASECAMP/Like Lion FE3/TIL/Week 06/10.06/026_조건문.js
@@ -0,0 +1,46 @@
+let money = 1000;
+let score = 89;   // 시험점수 89점
+
+let money = 1000;
+let score = 89;   // 시험점수 89점
+
+if (score > 90) {
+    console.log('mom: "i\'m so happy"');
+    money += 10000000;
+} else if (score > 80) {
+    console.log('mom: "i\'m happy"');
+    money += 100000;
+} else if (score > 70) {
+    console.log('mom: happy?');
+    money += 10000;
+} else if (score > 60) {
+    console.log('mom: happy!!!');
+} else {
+    console.log('mom: "..."');
+    money = 0;
+}
+
+
+
+// 예제2 
+
+// 처음부터 뺴앗고 시작
+if (money === 100000000) {
+    money -= 10000000
+}
+
+if (score > 90) {
+    console.log('mom: "i\'m so happy"');
+    money += 10000000;
+} else if (score > 80) {
+    console.log('mom: "i\'m so happy"');
+    money += 100000;
+} else if (score > 70) {
+    console.log('mom: "i\'m so happy"');
+    money += 10000;
+} else if (score > 60) {
+    console.log('mom: "i\'m so happy"');
+} else {
+    console.log('mom: "i\'m so happy"');
+    money = 0;
+}
