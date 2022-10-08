3c4ff5edfa69af390bde8541973db23ac1571f18
DaeYeob Kim <kimdyk2@hanmail.net>
2022-10-08 12:00:14 +0900

    인터페이스 콘솔 부분수정

--- Study/Publications/시원하게 긁어주는 TS/ch 03/03.interface3.ts
+++ Study/Publications/시원하게 긁어주는 TS/ch 03/03.interface3.ts
@@ -5,9 +5,9 @@ interface StudentInfo {
 }
 
 function printInfo(personA: StudentInfo) {
-  console.log(personA.name);
-  console.log(personA.grade);
-  console.log(personA.isAdult);
+  console.log(`이름: ${personA.name}`);
+  console.log(`학년: ${personA.grade}`);
+  console.log(`성인여부: ${personA.isAdult}`);
 }
 
 let myInfo = { name: "danny", grade: 4, isAdult: true };
