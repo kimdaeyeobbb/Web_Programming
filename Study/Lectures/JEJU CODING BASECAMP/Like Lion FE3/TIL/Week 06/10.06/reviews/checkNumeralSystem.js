e0d63cbb234a7ed2ab015b135b6678559a01734b
DaeYeob Kim <kimdyk2@hanmail.net>
2022-10-09 20:40:06 +0900

    check numeral system (is it octal?)

--- Study/Lectures/JEJU CODING BASECAMP/Like Lion FE3/TIL/Week 06/10.06/reviews/checkNumeralSystem.js
+++ Study/Lectures/JEJU CODING BASECAMP/Like Lion FE3/TIL/Week 06/10.06/reviews/checkNumeralSystem.js
@@ -1,4 +1,4 @@
-data = [010, 012, 1234, 5678]
+data = [0o10, 012, 1234, 5678]
 console.log(data[0].toString(10))   // 10진수로 변환시 8
 let result = data.join('-')
 console.log(result)   // 8-10-1234-5678
