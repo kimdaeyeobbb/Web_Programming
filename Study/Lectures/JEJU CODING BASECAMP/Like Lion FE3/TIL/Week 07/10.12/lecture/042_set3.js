5b966626da62f784babcb93f6ecfa74052f6d671
DaeYeob Kim <kimdyk2@hanmail.net>
2022-10-13 05:01:22 +0900

    set

--- Study/Lectures/JEJU CODING BASECAMP/Like Lion FE3/TIL/Week 07/10.12/lecture/042_set3.js
+++ Study/Lectures/JEJU CODING BASECAMP/Like Lion FE3/TIL/Week 07/10.12/lecture/042_set3.js
@@ -1,11 +1,15 @@
 let a = new Set('abc')
 let b = new Set('cde')
 
-// 교집합
-let cro = [...a].filter(value => b.has(value))
+/* 교집합 */
+let cro = [...a].filter((value) => b.has(value))
+console.log(cro)  // [ 'c' ]
 
-// 합집합
-let union = new Set([...a].concat(...b))
+/* 합집합 */
+let union = new Set([...a].concat(...b));
+console.log(union)  // Set(5) { 'a', 'b', 'c', 'd', 'e' }
 
-// 차집합
-let dif = new Set([...a].filter(x => !b.has(x)))
+
+/* 차집합 */
+let dif = [...a].filter((value) => !b.has(value))
+console.log(dif)   // [ 'a', 'b' ]
