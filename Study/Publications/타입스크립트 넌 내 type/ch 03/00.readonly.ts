5b2dd778b2a1361e59cd02f0b55b07f453772154
DaeYeob Kim <kimdyk2@hanmail.net>
2022-10-16 16:25:26 +0900

    readonly

---
+++ Study/Publications/타입스크립트 넌 내 type/ch 03/00.readonly.ts
@@ -0,0 +1,10 @@
+// 읽기 전용 배열
+let strArr: readonly string[] = ["가", "나", "다", "라"];
+console.log(strArr); // ["가", "나", "다", "라"];
+// strArr[0] = "A"; // error
+
+// 읽기 전용 배열
+let numArr: ReadonlyArray<number> = [1, 2, 3, 4, 5, 6];
+console.log(numArr); // [ 1, 2, 3, 4, 5, 6 ]
+
+// stoneArr.push(7); // error
