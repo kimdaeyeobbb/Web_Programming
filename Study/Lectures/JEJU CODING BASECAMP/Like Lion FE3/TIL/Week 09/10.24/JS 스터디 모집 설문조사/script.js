cd17c927adeff9fbfb414368601e98de3bcab45d
DaeYeob Kim <kimdyk2@hanmail.net>
2022-10-24 14:54:03 +0900

    22.10.24 수업

---
+++ Study/Lectures/JEJU CODING BASECAMP/Like Lion FE3/TIL/Week 09/10.24/JS 스터디 모집 설문조사/script.js
@@ -0,0 +1,66 @@
+const mainForm = document.querySelector("#mainForm");
+const progressBar = mainForm.querySelector('.bar-progress');  // mainForm의 자식요소들 중 progressBar를 선택
+const message = mainForm.querySelector('.msg-notice');
+const inputs = mainForm.querySelectorAll('input');
+const btnReset = mainForm.querySelector('button[type="reset"]'); // 속성 선택자를 사용. 속성 선택자는 대괄호로 감싸줌. reset이라는 속성을 가진 type 선택
+
+
+mainForm.addEventListener('keyup', () => {
+    // let validCounter = 0;// 유효한 input의 개수 저장
+
+    // inputs.forEach((item) => {
+    //     if (item.validity.valid) { // validity: 요소 내의 값이 유효한지 아닌지를 알려주는 여러가지 정보를 포함
+    //         validCounter++;
+    //     }
+    //})
+
+    /* filter를 사용할 경우 forEach와 counting 변수가 불필요함*/
+    const result = Array.prototype.filter.call(inputs, (item) => {
+        return item.validity.valid
+    });   // result에는 통과된 요소만 모아서 가지고 있음
+
+    /* 아래의 코드를 이용해도 됨 */
+    // const result = Array.from(inputs).filter((item) => {
+    //     return item.validity.valid
+    // })
+
+    console.log(result);
+
+
+    switch (result.length) {
+        case 1:
+            message.textContent = "좋습니다! 계속 진행하세요!";
+            progressBar.setAttribute('value', 20);
+            break;
+
+        case 2:
+            message.textContent = "좋은 답변이군요! 계속 진행하세요!";
+            progressBar.setAttribute('value', 40);
+            break;
+
+        case 3:
+            message.textContent = "절반 이상 지났습니다! 계속 진행하세요!";
+            progressBar.setAttribute('value', 60);
+            break;
+
+        case 4:
+            message.textContent = "거의 완료되었습니다! 계속 진행하세요!";
+            progressBar.setAttribute('value', 80);
+            break;
+
+        case 5:
+            message.textContent = "이제 제출해주세요!";
+            progressBar.setAttribute('value', 100);
+            break;
+
+        default:
+            message.textContent = "설문지를 작성해주세요!";
+            progressBar.setAttribute('value', 0);
+
+    }
+})
+
+btnReset.addEventListener('click', () => {
+    message.textContent = "설문지를 작성해주세요!";
+    progressBar.setAttribute('value', 0);
+})
--- Study/Lectures/JEJU CODING BASECAMP/Like Lion FE3/TIL/Week 09/10.24/Readme.md
+++ Study/Lectures/JEJU CODING BASECAMP/Like Lion FE3/TIL/Week 09/10.24/Readme.md
@@ -1,1 +1,41 @@
+# 22.10.24 (월)
 
+## fieldset
+
+- 필수사항이 아닌 선택사항
+
+## legend
+
+- 반드시 fieldset의 첫번째 자식 요소
+
+## [progress](https://developer.mozilla.org/ko/docs/Web/HTML/Element/progress)
+
+- a
+
+## [컬러 팔레트]((https://colorhunt.co/)
+
+## float
+
+- float을 썼을때 부모가 자식을 인지하지 못한다.
+
+- fieldset을 썼을 때 버튼의 존재를 아는 이유?
+
+  - fieldset은 block format context를 사용함
+  - (block format context 찾아보기)
+
+- [참고자료](https://html.spec.whatwg.org/multipage/rendering.html#the-fieldset-and-legend-elements)
+
+- float을 사용하면 따로 ~할 필요가 없다
+
+## filter
+
+- filter는 array의 메서드임. 노드리스트의 메서드가 아니다.
+
+## 노드리스트
+
+- 노드리스트 객체에는 forEach를 사용할 수 있으나 filter는 사용할 수 없다
+- call을 사용하면 노드리스트가 array의 filter를 빌려올 수 있다
+
+## 질문
+
+- Array.prototype.call이랑 [...inputs].filter는 동일한가?
