0817604b7387336331f6713fa3674fbcf9a6bc86
DaeYeob Kim <kimdyk2@hanmail.net>
2022-10-27 12:59:18 +0900

    todolist

--- Study/Lectures/JEJU CODING BASECAMP/Like Lion FE3/TIL/Week 09/10.26/109_todolist2.js
+++ Study/Lectures/JEJU CODING BASECAMP/Like Lion FE3/TIL/Week 09/10.26/109_todolist2.js
@@ -1,3 +1,9 @@
+/* 간단한 투두 리스트기능 구현해보기 */
+// 1. 해야할 일 Todo 클래스(내용, 상태 두 가지 프로퍼티를 가집니다.)
+// 2. Todo 클래스는 상태를 변경하는 changeState 메서드를 가집니다.
+// 3. 할일의 목록을 관리하는 관리자를 추상화한 TodoManager 클래스 (할일 목록을 프로퍼티로 가집니다.)
+// 4. 할 일을 저장하고(addItem), 할 일의 목록을 보여주며(getItems), 할 일의 남은 갯수를 반환하는(getLeftTodoCount) 세 가지 메소드를 가집니다.
+
 class Todo {
     constructor(item, finished) {
         this.item = item
@@ -44,11 +50,14 @@ class ToDoManager {
     }
 }
 
-const manager = new TodoManager();   // 인스턴스 생성
+const manager = new ToDoManager();   // 인스턴스 생성
 console.log(manager);   // TodoManager {todolist: Array(0)}
 
 manager.addItem('노래부르기!');
 console.log(manager.getItems());    // [ToDo]
 
 manager.getItems()[0].finished = true;
-console.log(manager.getLeftTodoCount());   // 
+console.log(manager.getLeftTodoCount());  // 0
+
+manager.getItems()[0].finished = false;
+console.log(manager.getLeftTodoCount());   // 1
