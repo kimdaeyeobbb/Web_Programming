e62a0657765824b62a526f8dd3d6ac26bf0c5cd2
DaeYeob Kim <kimdyk2@hanmail.net>
2022-10-27 15:14:47 +0900

    three.js practice

---
+++ Study/Lectures/JEJU CODING BASECAMP/Like Lion FE3/TIL/Week 09/10.27/threejs-lion/code/src/SampleModel.js
@@ -0,0 +1,36 @@
+export class SampleModel {
+	constructor(info = {}) {
+		this.scene = info.scene;
+		this.gltfLoader = info.gltfLoader;
+		this.width = info.width || 1;
+		this.height = info.height || 4;
+		this.depth = info.depth || 0.2;
+		this.scale = info.scale || 2;
+
+		this.x = info.x || 0;
+		this.y = info.y || this.height * 0.5;
+		this.z = info.z || 0;
+
+		this.modelSrc = info.modelSrc;
+
+		this.rotationY = info.rotationY || 0;
+
+		this.gltfLoader.load(
+			this.modelSrc,
+			glb => {
+				glb.scene.traverse(child => {
+					if (child.isMesh) {
+						child.castShadow = true;
+					}
+				});
+
+				this.mesh = glb.scene.children[0];
+				this.mesh.scale.set(this.scale, this.scale, this.scale);
+				this.mesh.castShadow = true;
+				this.mesh.rotation.y = this.rotationY;
+				this.mesh.position.set(this.x, this.y, this.z);
+				this.scene.add(this.mesh);
+			}
+		);
+	}
+}
