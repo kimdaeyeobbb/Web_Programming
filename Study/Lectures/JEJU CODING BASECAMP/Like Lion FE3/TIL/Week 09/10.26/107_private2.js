3eab502054d69a2ccb91998cfdcf1a2cdbcd29f8
DaeYeob Kim <kimdyk2@hanmail.net>
2022-10-27 12:59:09 +0900

    private

--- Study/Lectures/JEJU CODING BASECAMP/Like Lion FE3/TIL/Week 09/10.26/107_private2.js
+++ Study/Lectures/JEJU CODING BASECAMP/Like Lion FE3/TIL/Week 09/10.26/107_private2.js
@@ -21,5 +21,21 @@ const person = new PersonFunc();   // 생성자 함수의 실행결과인 innerP
 
 console.log(person)   // innerPerson {}
 console.log(person.getAge())  // 30
-console.log(person.age)   // 비공개 프로퍼티가 되어 더이상 사용 불가
+console.log(person.age)
+// 비공개 프로퍼티가 되어 더이상 사용 불가(옳지 않은설명)
+// 점 연산자를 통한 직접 접근이 불가능함  (옳은 설명)
+// (age 변수가 클로저 공간에 있으므로 innerPerson의 getAge 메서드만이 age에 접근할 수 있는 유일한 방법)
 
+
+/**
+ * 
+ * 비공개 프로퍼티가 되면 더 이상 사용이 불가능한것이 아닌,
+
+점 연산자를 통한 직접적으로 접근이 불가능하다라고 표현하는게 올바른것 같습니다!
+
+age 변수가 클로져 공간에 있기 때문에 innerPerson의 getAge 메서드만이 age에 접근할 수 있는 유일한 방법이 되죠?
+ 
+마치 class 문법에서 #키워드로 비공개 프로퍼티를 만들고 getter 메서드를 통해서만 접근할 수 있는 모습과 비슷합니다.
+
+때문에 이에 대응되는 방법으로 클로져를 이용한 사용자 정의 타입 패턴을 보여드린것입니다 🙂
+ */
