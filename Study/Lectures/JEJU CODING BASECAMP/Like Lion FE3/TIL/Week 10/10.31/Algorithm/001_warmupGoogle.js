b2c2d6fcba6341fa311f133d68509d9969107a35
DaeYeob Kim <kimdyk2@hanmail.net>
2022-10-31 11:16:58 +0900

    구글입사문제

--- Study/Lectures/JEJU CODING BASECAMP/Like Lion FE3/TIL/Week 10/10.31/Algorithm/001_warmupGoogle.js
+++ Study/Lectures/JEJU CODING BASECAMP/Like Lion FE3/TIL/Week 10/10.31/Algorithm/001_warmupGoogle.js
@@ -14,3 +14,18 @@ console.log(countingEight(10000))
 
 const countingEight2 = num => Array(10000).fill(1).map((n, idx) => n + idx).join('').match(/8/g).length
 console.log(countingEight2(10000))
+
+
+const countingEight3 = num => [...Array(num)].map((_, idx) => idx + 1).toString().match(/8/g).length
+console.log(countingEight3(10000))
+
+
+function countingEight4(num) {
+    let arr = []
+    for (let i = 1; i <= 10000; i++) {
+        arr.push(i)
+    }
+    let result = [...arr.join('')].filter(el => el === '8').length
+    return result
+}
+console.log(countingEight4(10000))
