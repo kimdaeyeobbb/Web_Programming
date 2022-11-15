106d3c84c55ec1e5053d13db633573576635b293
DaeYeob Kim <kimdyk2@hanmail.net>
2022-11-15 16:07:10 +0900

    feat: module css

---
+++ Study/Lectures/JejuCodingBasecamp/LikeLionFE3/TIL/Week 12/11.15/React/027_module_css/my-app/src/components/Two/Two.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import './two.css'
+// import './two.module.css'
+import './two.module.css'
+import styles from './two.module.css'
+
+export default function Two() {
+    return (
+        <section>
+            <h2>hello world</h2>
+            {/* <p className='contents'>hello world</p> */}
+            <p className={styles.contents}>hello world</p>
+        </section>
+    )
+}
