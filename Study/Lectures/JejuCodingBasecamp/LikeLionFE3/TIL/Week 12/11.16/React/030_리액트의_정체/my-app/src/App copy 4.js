d9f53df5ca07dd25399cf0f4e16fd5441b943eef
DaeYeob Kim <kimdyk2@hanmail.net>
2022-11-16 17:19:27 +0900

    feat: react's identity

---
+++ Study/Lectures/JejuCodingBasecamp/LikeLionFE3/TIL/Week 12/11.16/React/030_리액트의_정체/my-app/src/App copy 4.js
@@ -0,0 +1,19 @@
+function App() {
+  const Test = <h1>hello 1</h1>
+  console.log(Test)   // object
+
+  const TestTwo = [<h1 key="one">hello 1</h1>, <p key="two">hello 1</p>]
+  console.log(TestTwo) // object
+
+  console.log(typeof (TestTwo))
+  console.dir(TestTwo)
+
+  return (
+    <div>
+      {/* {[<h1>hello 1</h1>]} */}
+      {[<h1 key="one">hello 1</h1>, <p key="two">hello 1</p>]}
+    </div>
+  )
+}
+
+export default App;
