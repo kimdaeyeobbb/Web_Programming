c76db0737a808d6c5b7e3efc81911eab7e6badb4
DaeYeob Kim <kimdyk2@hanmail.net>
2022-11-21 14:21:46 +0900

    feat: hook flow

--- Study/Lectures/JejuCodingBasecamp/LikeLionFE3/TIL/Week 13/11.21/React/036_lazy_initialize/my-app/src/App.jsx
+++ Study/Lectures/JejuCodingBasecamp/LikeLionFE3/TIL/Week 13/11.21/React/036_lazy_initialize/my-app/src/App.jsx
@@ -1,22 +1,69 @@
-import { useEffect, useState } from "react";
-
-function getName() {
-  console.log("오래기다리는 작업");
-  return "개리";
-}
+import React, { useEffect, useLayoutEffect, useState } from "react";
 
 function App() {
-  // const [name, setName] = useState(getName())
-  const [name, setName] = useState(getName);
   const [num, setNum] = useState(0);
 
+  const handleonClick = () => {
+    setNum(num + 1);
+  };
+
+  useLayoutEffect(() => {
+    console.log("useLayoutEffect 1");
+    return () => {
+      console.log("useLayoutEffect return_1");
+    };
+  }, []);
+
+  useLayoutEffect(() => {
+    console.log("useLayoutEffect 2");
+    return () => {
+      console.log("useLayoutEffect return_2");
+    };
+  });
+
+  useLayoutEffect(() => {
+    console.log("useLayoutEffect 3");
+    return () => {
+      console.log("useLayoutEffect return_3");
+    };
+  }, [num]);
+
+  useEffect(() => {
+    console.log("useEffect 1");
+    return () => {
+      console.log("useEffect return_1");
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("useEffect 2");
+    return () => {
+      console.log("useEffect return_2");
+    };
+  });
+
+  useEffect(() => {
+    console.log("useEffect 3");
+    return () => {
+      console.log("useEffect return_3");
+    };
+  }, [num]);
+
+  return <button onClick={handleonClick}>{num}</button>;
+}
+
+const Wrap = () => {
+  const [isVisible, setIsVisible] = useState(true);
+  const handleClick = () => setIsVisible(!isVisible);
   return (
     <>
-      <div>
-        안녕하세요 {name}! 현재 숫자는 {num} 입니다!
-      </div>
-      <button onClick={() => setNum((p) => p + 1)}>{num}</button>
+      <button onClick={handleClick}>
+        {isVisible ? "언마운트시키기" : "마운트시키기"}
+      </button>
+      <br></br>
+      {isVisible && <App />}
     </>
   );
-}
-export default App;
+};
+
+export default Wrap;
