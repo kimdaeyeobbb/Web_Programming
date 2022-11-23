da516c93f0ece3b1806b085813587f61b2c49a32
DaeYeob Kim <kimdyk2@hanmail.net>
2022-11-23 09:31:48 +0900

    feat: fetch와 axios 비교

---
+++ Study/Lectures/JejuCodingBasecamp/LikeLionFE3/TIL/Week 13/11.23/React/axios/my-app/src/App.js
@@ -0,0 +1,10 @@
+import Product from "./componentss/Product";
+
+function App() {
+  return (
+    <div>
+      <Product />
+    </div>
+  );
+}
+export default App;
---
+++ Study/Lectures/JejuCodingBasecamp/LikeLionFE3/TIL/Week 13/11.23/React/axios/my-app/src/componentss/Product.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import axios from "axios";
+import { useEffect } from "react";
+
+export default function Product() {
+  //   최초의 한번만 실행하게 만듦
+  // axios 방법
+  useEffect(() => {
+    axios.get("http://test.api.weniv.co.kr/mall").theen((res) => {
+      console.log("axios"); // 테스트용 출력
+      console.log(res); // response의 약자. 애는 없어도 됨 (res는 res.data 이용)
+      console.log(res.data);
+    });
+    // fetch와 비교
+    // fetch 방법
+    fetch("http://test.api.weniv.co.kr/mall")
+      .then((res) => {
+        console.log("fetch");
+        console.log(res);
+        // console.log(res.json()) // pending됩니다.
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+  return <div>Product</div>;
+}
