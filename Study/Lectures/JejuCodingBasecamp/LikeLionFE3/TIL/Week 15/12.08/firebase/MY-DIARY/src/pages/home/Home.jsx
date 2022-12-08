4c7e272be26aadfaa9d36b98114b00f9c5bf1fef
DaeYeob Kim <kimdyk2@hanmail.net>
2022-12-08 13:52:35 +0900

    feat: useCollection

--- Study/Lectures/JejuCodingBasecamp/LikeLionFE3/TIL/Week 15/12.08/firebase/MY-DIARY/src/pages/home/Home.jsx
+++ Study/Lectures/JejuCodingBasecamp/LikeLionFE3/TIL/Week 15/12.08/firebase/MY-DIARY/src/pages/home/Home.jsx
@@ -6,7 +6,13 @@ import { useCollection } from "./../../hooks/useCollection";
 
 export default function Home() {
   const { user } = useAuthContext();
-  const { documents, error } = useCollection("myDiary");
+  const { documents, error } = useCollection("myDiary", [
+    "uid",
+    "==",
+    user.uid,
+  ]);
+  // collection이 가지고 있는 document 내에 작성한 유저의 아이디(uid)가 현재 로그인한 유저(user.uid)가 작성한 collection을 불러오겠다는 것
+  // 2가지가 같은 것만 불러와라는 의미
 
   return (
     <main className={styles.cont}>
