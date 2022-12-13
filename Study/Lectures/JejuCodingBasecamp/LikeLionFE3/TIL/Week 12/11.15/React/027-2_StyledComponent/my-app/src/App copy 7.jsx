975f6d109b21363c71a40430a0e9012ae00f00d7
DaeYeob Kim <kimdyk2@hanmail.net>
2022-12-13 15:21:34 +0900

    feat: styled-component

---
+++ Study/Lectures/JejuCodingBasecamp/LikeLionFE3/TIL/Week 12/11.15/React/027-2_StyledComponent/my-app/src/App copy 7.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+const ContentsDivOne = styled.div`
+  color: red;
+`;
+const ContentsDivTwo = styled.div`
+  color: green;
+  /* props가 들어오면 props.size를 반환 */
+  /* styled의 div가 메서드이기 때문에 가능한 것임 */
+  /* styled.div라는 메서드를 ContentsDivTwo라는 메서드로 사용할 수 있다
+  -> function ContentsDivTwo(props){ ...}으로 선언한 메서드와 같은 것 */
+  font-size: ${(props) => props.size + "px"};
+`;
+
+const Three = styled.div`
+  color: ${(옵션) => (옵션.option === "하나" ? "red" : "pink")};
+`;
+
+const Danny = styled.div`
+  font-size: ${(여비) => 여비.size + "px"};
+`;
+
+function App() {
+  return (
+    <>
+      <ContentsDivOne>hello</ContentsDivOne>
+      <ContentsDivTwo size={32}>world</ContentsDivTwo>
+      <Three option={"하나"}>korea</Three>
+      <Danny size={50}>크크크</Danny>
+    </>
+  );
+}
+
+export default App;
