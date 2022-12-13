2e8f3ff52410135684e291c9742f19a222a03b98
DaeYeob Kim <kimdyk2@hanmail.net>
2022-12-13 13:14:33 +0900

    feat: styled-component

---
+++ Study/Lectures/JejuCodingBasecamp/LikeLionFE3/TIL/Week 12/11.15/React/027-2_StyledComponent/my-app/src/App.jsx
@@ -0,0 +1,67 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import styled from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  /* reset css 적용 */
+  ${reset}
+  span {
+    color: red;
+    font-size: 12px;
+    }
+
+    a{
+        text-decoration : none;
+        color : inherit;
+    }
+
+    button{
+        border : none;
+        cursor : pointer;
+    }
+
+    * {
+    box-sizing: border-box;
+    }
+`;
+
+/* 스타일드 컴포넌트를 이용해서 스타일 적용하기 */
+
+const ContentDiv = styled.div`
+  // 전체를 감싸는 컨테이너의 스타일
+  margin: 40px;
+  border: 1px solid black;
+`;
+
+const ContentH2 = styled.h2`
+  color: ${({ name }) => (name === "zzza" ? "red" : "blue")}
+  width: 200px;
+  margin: 0 auto;
+  text-align: center;
+`;
+
+const ContentOne = styled.div`
+  margin: 40px;
+  color: green;
+`;
+
+const ContentTwo = styled(ContentOne)`
+  border: 1px solid aqua;
+`;
+
+function App() {
+  return (
+    <ContentDiv>
+      <ContentH2 name="zzza">Q&A</ContentH2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+        excepturi eius omnis beatae reiciendis a, placeat sed explicabo qui
+        similique aspernatur maxime mollitia aliquam libero voluptates?
+        Laboriosam rem nobis modi.
+      </p>
+      <ContentOne>Hleoo world 1</ContentOne>
+      <ContentTwo>Hleoo world 2</ContentTwo>
+    </ContentDiv>
+  );
+}
+export default App;
---
+++ Study/Lectures/JejuCodingBasecamp/LikeLionFE3/TIL/Week 12/11.15/React/027-2_StyledComponent/my-app/src/components/Example.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Example() {
+  return (
+    <div>
+      <h1>hello world 3</h1>
+      <span>hello world 4</span>
+    </div>
+  );
+}
