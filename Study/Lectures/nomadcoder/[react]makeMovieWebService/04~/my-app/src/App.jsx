2fd166b5e0eb0e3bfc7dad014cba9233e2c6fd3f
DaeYeob Kim <kimdyk2@hanmail.net>
2023-01-02 23:07:38 +0900

    feat: useEffect

--- Study/Lectures/nomadcoder/[react]makeMovieWebService/04~/my-app/src/App.jsx
+++ Study/Lectures/nomadcoder/[react]makeMovieWebService/04~/my-app/src/App.jsx
@@ -1,11 +1,45 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState(""); // keyword: state, setKeyword: function
+
+  const onClick = () => {
+    setValue((prev) => prev + 1);
+  };
+
+  const onChange = (e) => {
+    setKeyword(e.target.value);
+  };
+
+  useEffect(() => {
+    console.log("I run only once");
+  }, []);
+
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+
+  /* keyword와 counter가 동시에 변하는 경우 */
+  useEffect(() => {
+    console.log("I run when keyword & counter change");
+  }, [keyword, counter]);
+
   return (
     <div>
-      <h1 className="styles.title">Welcome back!!!</h1>
-      <Button text={"Continue"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
