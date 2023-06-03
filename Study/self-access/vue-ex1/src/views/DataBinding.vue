cb748d1ad52313954b0876432978f4770f0e95b7
Danny <kimdyk2@hanmail.net>
2023-06-03 21:41:56 +0900

    feat: data binding

---
+++ Study/self-access/vue-ex1/src/views/DataBinding.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>Hello, {{ title }}</div>
+</template>
+<script>
+export default {
+  components: {}, // 외부 컴포넌트 사용시 import후 배열로 등록
+  data() {
+    // html,js에서 전역 변수로 사용할 변수 선언
+    return {
+      title: "맨유",
+    };
+  },
+  setup() {}, // Composition API
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeUnmount() {},
+  unmounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped></style>
---
+++ Study/self-access/vue-ex1/src/views/DataBindingHtml.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>
+      <div v-html="htmlString"></div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      htmlString: '<p style="color:red;">This is red String</p>',
+    };
+  },
+};
+</script>
+
+<style scoped></style>
