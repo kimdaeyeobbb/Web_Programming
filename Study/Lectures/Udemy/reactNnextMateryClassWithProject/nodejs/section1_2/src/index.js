51fc9c11a70f5bdc7ec939c48bf04c5c5bc6b5e3
kimdaeyeobbb <kimdyk2@hanmail.net>
2024-03-07 17:50:51 +0900

    feature: nodejs > ESM

---
+++ Study/Lectures/Udemy/reactNnextMateryClassWithProject/nodejs/section1_2/src/index.js
@@ -0,0 +1,5 @@
+// ESM
+import multiply, { add, sub } from './math.js'
+console.log(add(1, 2)) // 3
+console.log(sub(1, 2)) // -1
+console.log(multiply(1, 2)) // 2
---
+++ Study/Lectures/Udemy/reactNnextMateryClassWithProject/nodejs/section1_2/src/math.js
@@ -0,0 +1,15 @@
+export function add(a,b) {
+    return a+b
+}
+
+export function sub(a,b) {
+    return a-b;
+}
+
+const multiply = (a,b) =>  {
+    return a*b;
+}
+
+export default multiply;
+
+// ESM(ECMAScript Module) 방식
